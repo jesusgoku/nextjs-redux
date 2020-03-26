d2020c4974d6e851bdd59d978017a54aaba3d6b7
Jesús Urrutia <jesus.urrutia@gmail.com>
2020-03-26 20:18:36 -0300

    chore: setting eslint for Next project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,15 @@
 module.exports = {
   extends: ["./node_modules/poetic/config/eslint/eslint-config.js"],
   // Add custom rules here
-  rules: {}
+  rules: {
+    'react/jsx-filename-extension': 0,
+  },
+  overrides: [
+    {
+      files: ['src/pages/**/*'],
+      rules: {
+        'react/react-in-jsx-scope': 0,
+      },
+    },
+  ],
 };
