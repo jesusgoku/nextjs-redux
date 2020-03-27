7edfce4f33067ea39e946397c508fd2d9d6f7fef
Jesús Urrutia <jesus.urrutia@gmail.com>
2020-03-26 22:46:30 -0300

    feat: add global styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,8 @@ import withRedux from 'next-redux-wrapper';
 
 import store from '@app/store';
 
+import '@app/styles/index.css';
+
 // eslint-disable-next-line no-shadow
 function App({ Component, pageProps, store }) {
   return (
